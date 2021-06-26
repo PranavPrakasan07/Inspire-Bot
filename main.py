c792138de8c5d13ebbf539c79afb7a4ad9381bbf
Pranav Prakasan <59109508+PranavPrakasan07@users.noreply.github.com>
2021-06-26 07:26:18 +0000

    added basic response

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import discord
+import os
+
+client = discord.Client()
+my_secret = os.environ['BOT_TOKEN']
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
+
+client.run(my_secret)
