e6367d687389b6c04437368a6df2f4f830a5d75d
Pranav Prakasan <59109508+PranavPrakasan07@users.noreply.github.com>
2021-06-26 08:12:31 +0000

    Quotes API used

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import discord
 import os
+import requests
+import json
 
 client = discord.Client()
 my_secret = os.environ['BOT_TOKEN']
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " - " + json_data[0]['a']
+  return quote
 
 @client.event
 async def on_ready():
@@ -13,7 +21,11 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
+  if message.content.startswith('$hello'):            
     await message.channel.send('Hello!')
+
+
+  if message.content.startswith('$inspire'):
+    await message.channel.send(get_quote())
 
 client.run(my_secret)
